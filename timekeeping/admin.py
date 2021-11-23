4d55138002f92afc1c1209f2d1b62e62a9429207
Philipp Rollinger <philipp.rollinger@gmail.com>
2021-11-23 14:00:12 +0100

    Assignment Model & Admin

--- timekeeping/admin.py
+++ timekeeping/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
-from timekeeping.models import Project, Task
+from timekeeping.models import Project, Task, Assignment
 
 class TaskInline(admin.TabularInline):
     model = Task
     fk_name = "project"
     show_change_link = True
-    #autocomplete_fields = ["target"]
-    #ordering = ("-updated_at",)
     extra = 1
     fields = ("title", "description", "status",)
 
@@ -17,6 +15,7 @@ class TaskInline(admin.TabularInline):
 class ProjectAdmin(admin.ModelAdmin):
 	""" Admin for Projects
 	"""
+	save_on_top = True
 	list_display = (
 		"id",
 		"owner",
@@ -57,10 +56,18 @@ class ProjectAdmin(admin.ModelAdmin):
         )
 	)
 
+class AssignmentInline(admin.TabularInline):
+    model = Assignment
+    fk_name = "task"
+    show_change_link = True
+    extra = 1
+    fields = ("user", "allowed", "max_workload",)
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
 	""" Admin for Tasks
 	"""
+	save_on_top = True
 	list_display = (
 		"id",
 		"project",
@@ -70,13 +77,19 @@ class TaskAdmin(admin.ModelAdmin):
 	)
 	list_display_links = ("title",)
 	readonly_fields = [
-        "id",
-        "created_at",
-        "updated_at",
-    ]
+		"id",
+		"created_at",
+		"updated_at",
+	]
 	search_fields = ["title", "description"]
 	autocomplete_fields = ["project",]
 	list_filter = ("status",)
+	list_editable = (
+		"status",
+	)
+	inlines = [
+		AssignmentInline,
+	]
 	prepopulated_fields = {"slug": ("title",)}
 	fieldsets = (
         (
@@ -99,3 +112,49 @@ class TaskAdmin(admin.ModelAdmin):
             },
         )
 	)
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    """ Admin for Assignments
+	"""
+    save_on_top = True
+    list_display = (
+        "id",
+        "__str__",
+		"max_workload",
+		"allowed"
+	)
+    list_display_links = ("__str__",)
+    readonly_fields = [
+        "id",
+        "created_at",
+        "updated_at",
+    ]
+    search_fields = ["task", "user"]
+    autocomplete_fields = ["task", "user"]
+    list_filter = ("allowed","max_workload",)
+    list_editable = (
+        "allowed",
+        "max_workload",
+    )
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    ("task", "user"),
+                    ("max_workload", "allowed"),
+                )
+            },
+        ),
+        (
+            _("System Information"),
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    ("id",),
+                    ("created_at", "updated_at"),
+                ),
+            },
+        )
+    )
--- timekeeping/models.py
+++ timekeeping/models.py
@@ -33,6 +33,7 @@ class Project(BaseModel):
 	class Meta:
 		verbose_name = _("Project")
 		verbose_name_plural = _("Projects")
+		ordering = ("title",)
 
 	owner = models.ForeignKey(
         User,
@@ -82,12 +83,11 @@ class Task(BaseModel):
 	class Meta:
 		verbose_name = _("Task")
 		verbose_name_plural = _("Tasks")
+		ordering = ("project", "title")
 
 	project = models.ForeignKey(
         Project,
         help_text=_("Task belongs to project"),
-        null=True,
-        blank=True,
         related_name="tasks",
         on_delete=models.CASCADE,
     )
@@ -126,3 +126,43 @@ class Task(BaseModel):
 		if not self.slug:
 			self.slug = slugify(self.title)
 		super(Task, self).save(*args, **kwargs)
+
+
+class Assignment(BaseModel):
+	""" Assignment Model
+	A User is assigned to a task of a project and can log work under the projects
+	"""
+
+	class Meta:
+		verbose_name = _("Assignment")
+		verbose_name_plural = _("Assignments")
+		ordering = ("task", "user")
+	
+	task = models.ForeignKey(
+        Task,
+        help_text=_("Task of assignment"),
+        related_name="assignments",
+        on_delete=models.CASCADE,
+    )
+	
+	user = models.ForeignKey(
+        User,
+        help_text=_("User of assignment"),
+        related_name="assignments",
+        on_delete=models.CASCADE,
+    )
+
+	allowed = models.BooleanField(
+		_("Allowed"),
+		help_text = _("Logging Work is allowed"), 
+		default=True
+	)
+
+	max_workload = models.PositiveSmallIntegerField(
+		_("Maximum Workload"),
+		help_text = _('Max workload in hours. 0 is unlimited hours'), 
+		default=0
+	)
+
+	def __str__(self):
+		return _("%s working on \"%s\"") % (self.user, self.task)
