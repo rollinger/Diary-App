3a0751dcdcd9245d88ed6419a67878ec5bec7959
Philipp Rollinger <philipp.rollinger@gmail.com>
2021-11-26 08:57:04 +0100

    Add auth doc

--- worklog/urls.py
+++ worklog/urls.py
@@ -21,9 +21,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
-    # API base url
+    # API Base Url
+    # See api_router for the routing of the viewsets
     path("api/", include("worklog.api_router")),
-    # DRF auth token
-    #path("auth-token/", obtain_auth_token),
-    path('api-auth/', include('rest_framework.urls'))
+    # DRF Auth Token
+    # POST username and password to this endpoint to get the authentication token.
+    # The token is used for subsequent requests from the client.
+    path("api-auth-token/", obtain_auth_token),
 ]
