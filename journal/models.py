7043583c79b542b4626e474a962502ff15a03993
Philipp Rollinger <philipp.rollinger@gmail.com>
2021-12-08 10:54:22 +0100

    Entry model setup

--- journal/models.py
+++ journal/models.py
@@ -21,3 +21,47 @@ class BaseModel(models.Model):
 	)
 	created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
 	updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+
+
+class Entry(BaseModel):
+	""" Entry Model
+	Defines an Journal entry. A text at a date from a user
+	
+	user: who made the entry
+	date: when the entry was made
+	text: the entry
+	emotions: emotion tags
+	"""
+
+	class Meta:
+		verbose_name = _("Entry")
+		verbose_name_plural = _("Entries")
+		ordering = ("-date",)
+		unique_together = ("user", "date")
+
+	user = models.ForeignKey(
+        User,
+        help_text=_("Author of the entry"),
+        null=True,
+        blank=True,
+        related_name="entries",
+        on_delete=models.CASCADE,
+    )
+
+	date = models.DateField(
+		_("Date"), 
+		help_text=_("Date the entry was made"),
+		auto_now_add=True
+	)
+
+	text = models.TextField(
+        _("Entry Text"),
+        help_text=_("The journal text"),
+        max_length=5000,
+    )
+
+	def __str__(self):
+		return _("%s Entry from %s") % (self.user, self.date)
+
+	def save(self, *args, **kwargs):
+		super(Entry, self).save(*args, **kwargs)
